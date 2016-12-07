92d690125f959aaac246d3561e1a60f83d14130f
lirik0910 <lirik-vagabund@yandex.ru>
2016-12-07 11:31:51 +0200

    Rejected commit: Default

--- www/newssite/models/news.php
+++ www/newssite/models/news.php
@@ -11,5 +11,28 @@ function getNews()
         $data[] = $row;
     }
     return $data;
+}
 
+function getOneNew()
+{
+    connectDB();
+    $id = $_GET['id'];
+    $query = "SELECT * FROM news WHERE id=$id";
+    $res = mysql_query($query);
+    $data = [];
+    while (false !== $row = mysql_fetch_assoc($res)){
+        $data[] = $row;
+    }
+    return $data;
+}
+
+function addNew()
+{
+    connectDB();
+    $title = $_POST['title'];
+    $desc = $_POST['descript'];
+    $date = $_POST['date'];
+    $query = "INSERT INTO news (title, descript, date) VALUES ('$title', '$desc', '$date')";
+    $res = mysql_query($query);
+    return $res;
 }
