c0e70c335b02d9b3c162896f35b5b267d980d3fa
lirik0910 <lirik-vagabund@yandex.ru>
2016-12-07 11:32:10 +0200

    Rejected commit: Default

--- www/newssite/views/index.php
+++ www/newssite/views/index.php
@@ -8,7 +8,11 @@
 </head>
 <body>
 <?php foreach ($news as $new):?>
-    <?php echo $new['title']; ?>
+    <a href="/newssite/new.php?id=<?php echo $new['id']; ?>"><?php echo $new['title']; ?></a>
+    <p><?php echo $new['date'];  ?></p>
 <?php endforeach; ?>
+<br>
+<br>
+<a href="/newssite/addnew.php">Добавить новость</a>
 </body>
 </html>
